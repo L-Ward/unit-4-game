6b2be5a2d40a041dbca9799826189190177f180d
L-Ward <noreply>
2018-06-30 12:10:13 -0700

    added functionality to select player character.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,87 @@
 //Variables
-var characters = [
-    {
-        id: 0,
-        name: "Leonardo",
-        img: function () {
-            var img = new Image(140, 109);
-            img.src = "assets/images/Leonardo.png";
-            return img;
+var game = {
+    characters: [
+        {
+            name: "Leonardo",
+            id: 0,
+            img: function () {
+                var img = new Image(140, 109);
+                img.src = "assets/images/Leonardo.png";
+                return img;
+            },
+            health: 100,
+            attack: 5
         },
-        health: 100,
-        attack: 5
-    },
-    {
-        id: 1,
-        img: function () {
-            var img = new Image(140, 109);
-            img.src = "assets/images/Raphael.png";
-            return img;
+        {
+            name: "Raphael",
+            id: 1,
+            img: function () {
+                var img = new Image(140, 109);
+                img.src = "assets/images/Raphael.png";
+                return img;
+            },
+            health: 100,
+            attack: 5
         },
-        name: "Raphael",
-        health: 100,
-        attack: 5
-    },
-    {
-        id: 2,
-        img: function () {
-            var img = new Image(140, 109);
-            img.src = "assets/images/Donatello.png";
-            return img;
+        {
+            name: "Donatello",
+            id: 2,
+            img: function () {
+                var img = new Image(140, 109);
+                img.src = "assets/images/Donatello.png";
+                return img;
+            },
+            health: 100,
+            attack: 5
         },
-        name: "Donatello",
-        health: 100,
-        attack: 5
-    },
-    {
-        id: 3,
-        img: function () {
-            var img = new Image(140, 109);
-            img.src = "assets/images/Michaelangelo.png";
-            return img;
-        },
-        name: "Michaelangelo",
-        health: 100,
-        attack: 5
-    }
-]
-// var chosenCharacter;
-// var enemies;
-// var defender;
+        {
+            name: "Michaelangelo",
+            id: 3,
+            img: function () {
+                var img = new Image(140, 109);
+                img.src = "assets/images/Michaelangelo.png";
+                return img;
+            },
+            health: 100,
+            attack: 5
+        }
+    ],
+    playerCharacter: "",
+    enemies: [],
+    defender: "",
+    isPlayerCharacterChosen: false,
+    isDefenderChosen: false
+}
+
+
 
 //Assign characters to buttons
 var charMenu = $(".characterMenu")
-
-characters.forEach(function(character){
-    // charMenu.append("<btn>" + character.name + character.health + "</btn>")
+//loop through characters array to create buttons
+game.characters.forEach(function (character) {
     var button = document.createElement("button");
-    $(button).addClass("btn btn-" + character.id);
+    $(button).addClass("btn btn-character btn-" + character.id);
+    $(button).attr("value", character.id);
     $(button).text(character.name).append(character.img()).append(character.health);
     charMenu.append(button);
 })
 
+//intialize the game
+function initializeGame() {
+    game.playerCharacter = "";
+    game.enemies = [];
+    game.defender = "";
+    game.isPlayerCharacterChosen = "false";
+    game.isDefenderChosen = "false"
+}
 
+//choose a player character
+$(".btn-character").on("click", function () {
+    if (game.isPlayerCharacterChosen === false) {
+        game.isPlayerCharacterChosen = true;
+        game.playerCharacter = $(this).val();
+        console.log(game.playerCharacter);
+        console.log(game.isPlayerCharacterChosen);
+    }
+})
 
